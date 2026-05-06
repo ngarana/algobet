a29c79359465a25584715ad09b40919f78ca282c
namatanda <stanford.githimu@court.go.ke>
2026-05-06 18:26:56 +0300

    refactor: remove deprecated football-data importer and tests

--- algobet/importers/__init__.py
+++ algobet/importers/__init__.py
@@ -1,11 +1,9 @@
 """Data importers for external football data sources.
 
 This package provides importers for various football data sources:
-- Football-Data.co.uk: Free historical match data and betting odds
 - SoccerData: Unified scraper for FBref, WhoScored, and more
 """
 
-from algobet.importers.football_data import FootballDataImporter
 from algobet.importers.soccerdata_importer import SoccerDataImporter
 
-__all__ = ["FootballDataImporter", "SoccerDataImporter"]
+__all__ = ["SoccerDataImporter"]
